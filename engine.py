4ebe955be344a33a657476d49c822a91ef7739df
Adhi-2648 <AdhiNarayanan.Ramesh01@student.csulb.edu>
2024-11-03 14:29:39 -0800

    additional functions

--- engine.py
+++ engine.py
@@ -64,7 +64,7 @@ class Value:
         self.grad = 1
         for v in reversed(topo):
             v._backward()
-            
+
     def __neg__(self): # -self
         return self * -1
 
@@ -76,3 +76,15 @@ class Value:
 
     def __rsub__(self, other): # other - self
         return other + (-self)
+    
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+
+    def __rtruediv__(self, other): # other / self
+        return other * self**-1
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
